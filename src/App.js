143968c673a8dab87edf831b4c9ecab495ce714c
HAlGhanim <imzaji98@gmail.com>
2023-05-31 19:30:47 +0300

    created the wallet

--- src/App.js
+++ src/App.js
@@ -2,8 +2,13 @@ import logo from "./logo.svg";
 import "./App.css";
 import Clicks from "./components/Clicks";
 
+
 function App() {
-  return <Clicks />;
+  return (
+    <>
+      <Clicks />
+    </>
+  );
 }
 
 export default App;
--- src/components/Clicks.js
+++ src/components/Clicks.js
@@ -1,19 +1,26 @@
 import React, { useState } from "react";
 
 const Clicks = () => {
-  const [count, setCount] = useState(0);
-
+  const [count, setCount] = useState({
+    score: 0,
+    wallet: 0,
+  });
   const handleClick = (event) => {
     // setCount(event.target.value);
-    setCount((count) => count + 1);
+    const copy = { ...count };
+    copy.score += 1;
+    copy.wallet += 1;
+
+    setCount(copy);
     console.log(`clicks: ${event.target.value}`);
   };
   return (
     <div className="scoreButton">
+      <p>Score: {count.score}</p>
       <button type="button" onClick={handleClick}>
         Click Me
       </button>
-      <p>Score: {count} KD</p>
+      <div className="wallet">Money: {count.wallet} KD </div>
     </div>
   );
 };
