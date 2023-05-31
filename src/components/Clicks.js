b37d56fc71f5ed8ad213e00783997eff1953622a
HAlGhanim <imzaji98@gmail.com>
2023-05-31 20:29:47 +0300

    reached th conditional button but we haven't reached the button function

--- src/components/Clicks.js
+++ src/components/Clicks.js
@@ -5,23 +5,41 @@ const Clicks = () => {
     score: 0,
     wallet: 0,
   });
+  const copy = { ...count };
   const handleClick = (event) => {
     // setCount(event.target.value);
-    const copy = { ...count };
     copy.score += 1;
     copy.wallet += 1;
 
     setCount(copy);
     console.log(`clicks: ${event.target.value}`);
   };
+  const purchase = () => {
+    if (upgradeButton) {
+      count.wallet -= 10;
+    }
+  };
+  const upgradeButton = () => {
+    if (count.wallet >= 10) {
+      return (
+        <button type="button" onClick={purchase}>
+          purchase double points for 10KD?
+        </button>
+      );
+    }
+  };
+
   return (
-    <div className="scoreButton">
-      <p>Score: {count.score}</p>
-      <button type="button" onClick={handleClick}>
-        Click Me
-      </button>
-      <div className="wallet">Money: {count.wallet} KD </div>
-    </div>
+    <>
+      <div className="scoreButton">
+        <p>Score: {count.score}</p>
+        <button type="button" onClick={handleClick}>
+          Click Me
+        </button>
+        <div className="wallet">Money: {count.wallet} KD </div>
+      </div>
+      <div>{upgradeButton()}</div>
+    </>
   );
 };
 
